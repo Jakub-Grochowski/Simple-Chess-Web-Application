eec65a90aea228be2226fcafbc7162b0f89f57ff
Jakub-Grochowski <grochowski.jakub96@gmail.com>
2021-08-08 13:43:15 +0200

    BackUp Point before view adn controllers will be added

--- Models/Board.cs
+++ Models/Board.cs
@@ -13,6 +13,7 @@ namespace Simple_Chess_Web_Application.Models
         public Board()
         {
             Size = 8;
+            theGrid = new Cell[Size, Size];
             for(int i = 0; i < Size; i++)
             {
                 for(int j = 0; j < Size; j++)
@@ -25,6 +26,7 @@ namespace Simple_Chess_Web_Application.Models
         public Board(int size)
         {
             Size = size;
+            theGrid = new Cell[Size, Size];
             for (int i = 0; i < Size; i++)
             {
                 for (int j = 0; j < Size; j++)
@@ -33,5 +35,47 @@ namespace Simple_Chess_Web_Application.Models
                 }
             }
         }
+
+        public void MarkNextLegalMoves(Cell currentCell,string chessPiece)
+        {
+            ClearBoard();
+
+            switch (chessPiece)
+            {
+                case "Knight":
+                    theGrid[currentCell.RowNumber+2, currentCell.ColumnNumber+1].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber+2, currentCell.ColumnNumber-1].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber-2, currentCell.ColumnNumber+1].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber-2, currentCell.ColumnNumber-1].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber+1, currentCell.ColumnNumber+2].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber+1, currentCell.ColumnNumber-2].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber-1, currentCell.ColumnNumber+2].IsLegalMove = true;
+                    theGrid[currentCell.RowNumber-1, currentCell.ColumnNumber-2].IsLegalMove = true;
+                    break;
+                case "King":
+                    break;
+                case "Rook":
+                    break;
+                case "Pawn":
+                    break;
+                case "Bishop":
+                    break;
+                case "Queen":
+                    break;
+
+            }
+        }
+        public void ClearBoard()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].IsLegalMove = false;
+                    theGrid[i, j].IsOccupaied = false;
+                }
+            }
+        }
+                
     }
 }
