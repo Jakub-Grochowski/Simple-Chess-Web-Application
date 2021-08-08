5bbdc074568f4c649582736c22367589cf0c862f
Jakub-Grochowski <grochowski.jakub96@gmail.com>
2021-08-08 12:40:56 +0200

    Basic Structure

---
+++ Models/Board.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple_Chess_Web_Application.Models
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] theGrid { get; set; }
+
+        public Board()
+        {
+            Size = 8;
+            for(int i = 0; i < Size; i++)
+            {
+                for(int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i,j);
+                }
+            }
+        }
+
+        public Board(int size)
+        {
+            Size = size;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+    }
+}
---
+++ Models/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple_Chess_Web_Application.Models
+{
+    public class Cell
+    {
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+        public bool IsLegalMove { get; set; }
+        public bool IsOccupaied { get; set; }
+
+        public Cell(int rowNumber, int columnNumber)
+        {
+            RowNumber = rowNumber;
+            ColumnNumber = columnNumber;
+            IsLegalMove = false;
+            IsOccupaied = false;
+        }
+    }
+}
